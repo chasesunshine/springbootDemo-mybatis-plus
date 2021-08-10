dad75a35bb44ac345d6f4a5c9b9119f8972638ee
马佳健 <13917186462@163.com>
2021-08-10 15:06:06 +0800

    commit

--- src/main/java/com/jt/demo/service/UserService.java
+++ src/main/java/com/jt/demo/service/UserService.java
@@ -1,10 +1,12 @@
 package com.jt.demo.service;
 
-import com.alibaba.druid.support.json.JSONUtils;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.jt.demo.convert.UserConver;
 import com.jt.demo.mapper.UserMapper;
 import com.jt.demo.pojo.User;
+import com.jt.demo.pojo.common.UserVo;
+import com.jt.demo.pojo.common.UserVo1;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private UserConver UserConver;
 
     public String selectOne() {
         User user = userMapper.selectOne(Wrappers.<User>query().lambda()
                 .eq(User::getId,7));
+        UserVo userVo = UserConver.item2Dto(user);
 
-        return JSON.toJSONString(user);
+        UserVo1 convert = UserConver.convert(user);
+
+        return JSON.toJSONString(userVo);
     }
 }
